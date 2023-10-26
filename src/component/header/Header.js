18129e88a465149d9a5046da6d56ca320f2c3244
DONGEM1 <ldem01111997@gmail.com>
2023-10-26 21:14:32 +0700

    update lan 2

---
+++ src/component/header/Header.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText,
+} from 'reactstrap';
+
+  
+export default function Header(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle1 = () => setIsOpen(!isOpen);
+    return (
+        <div>
+        <Navbar >
+          <NavbarBrand href="/">Home</NavbarBrand>
+          <NavbarBrand href="/">Home</NavbarBrand>
+          <NavbarToggler onClick={toggle1} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+}
